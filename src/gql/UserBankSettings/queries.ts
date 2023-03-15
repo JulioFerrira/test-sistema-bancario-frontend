7d9c6bdad20b23d81f149742b91660bab52e4bb3
Julio Ferreira <julioferreira@MacBook-Pro-de-Julio.local>
2023-03-15 00:11:57 -0300

    remove unused fields on query

--- src/gql/UserBankSettings/queries.ts
+++ src/gql/UserBankSettings/queries.ts
@@ -4,26 +4,6 @@ export const GET_USER_BANK_SETTINGS = gql`
     userBankSetting {
       bankSetting {
         id
-        contacts {
-          user {
-            name
-            email
-          }
-          bankAccount {
-            id
-            updatedAt
-            createdAt
-            balance
-            accountNumber
-            typeAccount
-            ... on CheckingAccount {
-              transferenceCost
-            }
-            ... on SavingsAccount {
-              interestRate
-            }
-          }
-        }
       }
       user {
         id
